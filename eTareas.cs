f153cd9ccbbe00be0ebcbf8e5adb5f212ca547c5
Ramón Alejandro Ruiz <alexestelar@yahoo.com.ar>
2023-06-15 15:54:11 -0300

    ejercicio 1

---
+++ eTareas.cs
@@ -0,0 +1,15 @@
+namespace EspacioTareas;
+
+public enum eTareas
+{
+    Estudiar = 1,
+    Limpiar = 2,
+    Cocinar = 3,
+    Programar = 4,
+    Pintar = 5,
+    Ordenar = 6,
+    Clasificar = 7,
+    Fiscalizar = 8,
+    Capitalizar = 9,
+    Sarasear = 10,
+}
